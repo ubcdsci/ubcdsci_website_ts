b39d92aae4e81c6b6e68c33d4a4176f6ef1cc2a6
jon-chow <jon.chowyyc@gmail.com>
2022-10-19 16:57:34 -0700

    set up backend

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config(); 
+
+
+// declare a route with a response
+app.get('/', (req, res) => {
+  res.send("Response");
+});
+
+// start the server
+app.listen(process.env.BACK_PORT, () => {
+  console.log(`Server running on: http://${process.env.BACK_HOST}:${process.env.BACK_PORT}`);
+});
