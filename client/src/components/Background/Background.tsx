55569e2ed1a9f736bee8ab4a054454bfc5801c38
Jon Chow <70170720+jon-chow@users.noreply.github.com>
2022-12-23 13:08:00 -0800

    added docs, added custom overlay opacity

--- client/src/components/Background/Background.tsx
+++ client/src/components/Background/Background.tsx
@@ -1,9 +1,16 @@
+// Style imports.
 import styles from './Background.module.scss';
 
-const Background = (props : any) => {
+/**
+ * Renders a Background.
+ * @param {string} src Image source.
+ * @param {number} opacity Opacity of the overlay.
+ * @returns {JSX.Element} JSX Component.
+ */
+const Background = (props : {src : string, opacity : number}) => {
   return (
     <div className={styles.Background}>
-      <div className={styles.Overlay}/>
+      <div className={styles.Overlay} style={{opacity: props.opacity ? props.opacity : "0.7"}} />
       
       <img
         className={styles.BackgroundImage}
