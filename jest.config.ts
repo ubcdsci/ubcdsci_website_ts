47cbce2fe9840de769902f5080c0b741a1196352
MuzammilC <105989297+MuzammilC@users.noreply.github.com>
2024-02-23 21:36:45 -0800

    Added divider

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { JestConfigWithTsJest } from "ts-jest";
-
-const JestConfig : JestConfigWithTsJest = {
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
-  },
-}
-
-export default JestConfig;
