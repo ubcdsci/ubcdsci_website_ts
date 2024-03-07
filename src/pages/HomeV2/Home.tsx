85400782976792018f5cd2af8928d140a1194402
adrianlamdev <me@adrianlam.dev>
2024-03-07 14:21:23 -0800

    feat(src/pages/HomeV2/Home.ts): Add scss and initial setup of hero

--- src/pages/HomeV2/Home.tsx
+++ src/pages/HomeV2/Home.tsx
@@ -1,9 +1,24 @@
-const Home = () => {
+// JSX Component
+import styles from "./HomeV2.module.scss";
 
-    return (
-        <>
-        </>
-    )
-}
+const Home = () => {
+  return (
+    <div className={styles.HeroContainer}>
+      <h1 className={styles.HeroTitle}>
+        An academic community for all your{" "}
+        <span className={styles.Statistical}>statistical</span> needs.
+      </h1>
+      <p className={styles.HeroDescription}>
+        Fostering future data science leaders through an inclusive community{" "}
+        that connects{" "}
+        <span className={styles.Highlight}>academia with industry.</span>
+      </p>
+      <div className={styles.EmailRegister}>
+        <input type="text" placeholder="Email" className={styles.EmailInput} />
+        <button className={styles.RegisterButton}>Register</button>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
