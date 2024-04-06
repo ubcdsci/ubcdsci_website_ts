85e2b40a198d17a7e9aef83c7f8b4ac283c728bc
adrianlamdev <me@adrianlam.dev>
2024-04-06 11:13:32 -0700

    feat: add styles to content

--- src/pages/HomeV2/Home.tsx
+++ src/pages/HomeV2/Home.tsx
@@ -4,6 +4,7 @@ import styles from "./HomeV2.module.scss";
 const Home = () => {
   return (
     <div className={styles.HeroContainer}>
+      <span className={styles.Label}>&#60;UBC DATASCIENCE CLUB /&#62;</span>
       <h1 className={styles.HeroTitle}>
         An academic community for all your{" "}
         <span className={styles.Statistical}>statistical</span> needs.
@@ -14,7 +15,7 @@ const Home = () => {
         <span className={styles.Highlight}>academia with industry.</span>
       </p>
       <div className={styles.EmailRegister}>
-        <input type="text" placeholder="Email" className={styles.EmailInput} />
+        <input type="text" placeholder="EMAIL" className={styles.EmailInput} />
         <button className={styles.RegisterButton}>Register</button>
       </div>
     </div>
