9be0d45de15318a04d1303ebe4084ee8b6ed6a60
Raymond Li <rli86@students.cs.ubc.ca>
2026-03-09 18:12:29 -0700

    fixed comments

--- src/@types/components.d.ts
+++ src/@types/components.d.ts
@@ -136,10 +136,9 @@ interface EventObject {
 
 interface UpcomingEventObject extends EventObject {
   important: boolean;
-  // optional precise datetimes (ISO string or Date)
   start?: string | Date;
   end?: string | Date;
-  timezone?: string; // IANA timezone like 'America/Los_Angeles'
+  timezone?: string; // IANA timezone
 }
 
 interface PastEventObject extends EventObject {
